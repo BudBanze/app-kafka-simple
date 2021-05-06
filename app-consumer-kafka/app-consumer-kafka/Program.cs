bf7185f9b1324cc7c58e4ef1b3e76b47fbdd0a7a
PRISCILA OLIVEIRA DA SILVA <priscilaosilva.dev@outlook.com>
2021-05-06 09:47:51 -0300

    projeto kafka producer e consumer simples

---
+++ app-consumer-kafka/app-consumer-kafka/Program.cs
@@ -0,0 +1,51 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace app_consumer_kafka
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("test-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Mensagem consumida: '{cr.Value}' De: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ app-producer-kafka/app-producer-kafka/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace app_producer_kafka
+{
+    static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            try
+            {
+                int cont = 0;
+
+                while (true)
+                {
+                    var dr = await p.ProduceAsync("test-topic",
+                            new Message<Null, string> { Value = $"test:  {cont++}" });
+
+
+                    Console.WriteLine($"A mensagem '{dr.Value}' entrega no topico '{dr.TopicPartitionOffset} | {cont}'");
+
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+            }
+        }
+    }
+}
